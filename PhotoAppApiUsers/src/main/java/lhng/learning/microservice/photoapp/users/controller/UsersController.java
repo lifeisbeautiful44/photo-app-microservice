3980f34cf571bada4a92c647bbdb693f2aeabffc
lifeisbeautiful44 <bohara.srijansil44@gmail.com>
2025-03-12 22:56:22 +0545

    [Microservice:] configuring multiple microserivce instance, with eureka-isntance-id

--- PhotoAppApiUsers/src/main/java/lhng/learning/microservice/photoapp/users/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/lhng/learning/microservice/photoapp/users/controller/UsersController.java
@@ -1,5 +1,7 @@
 package lhng.learning.microservice.photoapp.users.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")
 @RestController
 public class UsersController {
+    @Autowired
+    private Environment environment;
     @GetMapping("/status/check")
     public String status(){
-        return "Working";
+        return "Working on port: "+ environment.getProperty("local.server.port");
     }
 }
